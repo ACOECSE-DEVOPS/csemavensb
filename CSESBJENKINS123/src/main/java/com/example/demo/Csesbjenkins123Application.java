bd0d32bda619d611161dce57ab0639716c933dbe
ACOE DEVOPS <acoecolledecse@gmail.com>
2025-09-13 00:14:33 +0530

    secnd

--- CSESBJENKINS123/src/main/java/com/example/demo/Csesbjenkins123Application.java
+++ CSESBJENKINS123/src/main/java/com/example/demo/Csesbjenkins123Application.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Csesbjenkins123Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Csesbjenkins123Application.class, args);
+	}
+	@GetMapping("/mine")
+	public String disp() {
+		return "MINE WELOME";
 	}
 
 }
--- CSESBJENKINS123/src/main/java/com/example/demo/HomeController.java
+++ CSESBJENKINS123/src/main/java/com/example/demo/HomeController.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 	@GetMapping("/data")
 	public String rocks() {
-		return "WELCOME TO CSE";
+		return "WELCOME TO CSE from DP";
 	}
 
 }
